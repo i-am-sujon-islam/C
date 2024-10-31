e6139f89329840b93c2e08b27711de516c4b724c
Md Sujon Islam <iamsujonislam06@gmail.com>
2024-10-31 20:36:38 +0600

    Start programming solving by C language

---
+++ 1/first.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("MD Sujon Islam");
+    return 0;
+}
---
+++ 2/second.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+printf("Ok it is my second programming");
+return 0;
+
+}
